de90f0dc1f27d860a2939c391adf1ec0efed2a72
unknown <bd2747@columbia.edu>
2025-04-19 18:53:03 -0400

    Updated homepage

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# ---------------------- Home & Main Pages ----------------------
 @app.route('/')
 def home():
     return render_template('home.html')
