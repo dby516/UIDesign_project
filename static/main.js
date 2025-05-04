6815277d94c05258f87ad735badb84d75c9c0305
Cristina-H48 <hh3101@columbia.edu>
2025-05-04 00:13:14 -0400

    Change cheatsheet popup
    Refine some graphic design
    Refine cheatsheet.html
    Add connection between steps and sections

--- static/main.js
+++ static/main.js
@@ -13,3 +13,24 @@ function Quiz() {
 function StartPlayRound() {
     window.location.href = '/play_round/1';
 }
+function showCheatsheetPopup() {
+    const popup = document.getElementById('cheatsheet-popup');
+    const closeBtn = popup.querySelector('.popup-close');
+
+    // Make it visible
+    popup.classList.remove('hidden');
+    popup.classList.add('show');
+
+    // Allow manual close
+    closeBtn.onclick = () => {
+        popup.classList.remove('show');
+        setTimeout(() => popup.classList.add('hidden'), 500); // match CSS transition
+    };
+
+    // Auto close after 3 seconds
+    setTimeout(() => {
+        popup.classList.remove('show');
+        setTimeout(() => popup.classList.add('hidden'), 500); // match CSS transition
+    }, 3000);
+}
+
